efd643efc1832e91f840da57984810a13f354daa
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-07-07 14:50:52 +0100

    encrypted password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,6 +21,36 @@ const authUser = asyncHandler(async (req, res) => {
 	}
 });
 
+const registerUser = asyncHandler(async (req, res) => {
+	const { name, email, password } = req.body;
+
+	const userExists = await User.findOne({ email });
+
+	if (userExists) {
+		res.status(400);
+		throw new Error('User already exisits');
+	}
+
+	const user = await User.create({
+		name,
+		email,
+		password
+	});
+
+	if (user) {
+		res.status(201).json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+			token: generateToken(user._id)
+		});
+	} else {
+		res.status(400);
+		throw new Error('Invalid User data');
+	}
+});
+
 const getUserProfile = asyncHandler(async (req, res) => {
 	const user = await User.findById(req.user._id);
 
@@ -37,4 +67,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
 	}
 });
 
-export { authUser, getUserProfile };
+export { authUser, registerUser, getUserProfile };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -9,11 +9,12 @@ const userSchema = mongoose.Schema(
 		},
 		email: {
 			type: String,
-			required: true
+			required: true,
+			unique: true
 		},
 		password: {
 			type: String,
-			unique: true
+			required: true
 		},
 		isAdmin: {
 			type: Boolean,
@@ -29,6 +30,15 @@ const userSchema = mongoose.Schema(
 userSchema.methods.matchPassword = async function (enteredPassword) {
 	return await bcrypt.compare(enteredPassword, this.password);
 };
+
+userSchema.pre('save', async function (next) {
+	if (!this.isModified('password')) {
+		next();
+	}
+
+	const salt = await bcrypt.genSalt(10);
+	this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model('User', userSchema);
 

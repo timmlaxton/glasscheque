014feb34019c265b9165c2a8dc5746797e8e24c8
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-07-10 14:16:37 +0100

    added orderActions

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 dotenv.config();
 
@@ -11,55 +16,33 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-const nodemailer = "nodemailer";
-const path = "path";
-
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
-// app.get('/', (req, res) => {
-// 	res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
-// const contactEmail = nodemailer.createTransport({
-// 	service: 'gmail',
-// 	auth: {
-// 		user: process.env.NODEMAILER_USER,
-// 		pass: process.env.NODEMAILER_PASSWORD
-// 	}
-// });
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
 
-// contactEmail.verify((error) => {
-// 	if (error) {
-// 		console.log(error);
-// 	} else {
-// 		console.log('Ready to Send');
-// 	}
-// });
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-// app.post('/api/contact', (req, res) => {
-// 	const name = req.body.name;
-// 	const email = req.body.email;
-// 	const message = req.body.message;
-// 	const mail = {
-// 		from: name,
-// 		to: 'timmlaxton@gmail.com',
-// 		subject: 'Contact Form Submission',
-// 		html: `<p>Name: ${name}</p>
-//            <p>Email: ${email}</p>
-//            <p>Message: ${message}</p>`
-// 	};
-// 	contactEmail.sendMail(mail, (error) => {
-// 		if (error) {
-// 			res.json({ status: 'ERROR' });
-// 		} else {
-// 			res.json({ status: 'Thanks for getting in touch, I will get back to you soon' });
-// 		}
-// 	});
-// });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
